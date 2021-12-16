24d616ca4c50b8dd871006102514e8d0cdf60e72
David Gómez <93141341+DaGomezPerez@users.noreply.github.com>
2021-12-16 12:34:44 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,24 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+var moito = 9 ;
+    var raiz = Math.sqrt(moito);
+    alert(raiz);
+var morsa = 50;
+	var	cuadrado	=	Math.pow(morsa, 2)
+	alert(cuadrado)
+
+var hola = Math.PI;
+var tercero = Math.trunc(hola)
+alert(tercero)
+
+
+var bur= (x=200 , y=50)
+var cuarto=Math.min(x, y)
+alert(cuarto)
+
+var cer= (0-3)
+var quinto= Math.random(cer)
+alert(quinto)
