89e361f57c576de2f8c71fae2afb6c8f0b31761c
David Gómez <93141341+DaGomezPerez@users.noreply.github.com>
2021-12-17 12:29:51 +0100

    Add files via upload

---
+++ ejercicio5/escripito.js
@@ -0,0 +1,11 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+
+var numero
+	numero = prompt("cuantas docena quieres")
+alert("Tu eleccion son" + " " + numero)
+alert("la cantidad de huevos son" +" " + 12 * numero)
